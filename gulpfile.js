0f36338288daa1395fac9ad3518b194519884529
vitalikpetreniuk <petreniuk.ua@gmail.com>
2021-02-16 08:54:42 +0200

    removed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ let paths = {
 
 	scripts: {
 		src: [
-			'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
-			baseDir + '/js/owl.carousel.min.js',
+			//'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+			//baseDir + '/js/owl.carousel.min.js',
 			baseDir + '/js/app.js' // app.js. Always at the end
 		],
 		dest: baseDir + '/js',
